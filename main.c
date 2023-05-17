e2f251001615a6b7e188af8e22a7ea2bdaf29591
HidayatNurHakim <132598651+HidayatNurHakim@users.noreply.github.com>
2023-05-17 12:32:07 +0700

    update buat baca file di main.c

--- main.c
+++ main.c
@@ -2,4 +2,21 @@
 
 int main()
 {
+   FILE *fpw = fopen("database/login.bin", "wb");
+    char id[20];
+    char pw[20];
+    printf("Registrasi\n");
+    printf("Masukan id: ");
+    scanf("%s", id);
+
+    printf("Masukan pw: ");
+    scanf("%s", pw);
+    fprintf(fpw, "%s", id);
+
+    fprintf(fpw, " ");
+    fprintf(fpw, "%s", pw);
+
+    fclose(fpw);
+
+        return 0;
 }
